0c44548bf5ee704bc3ab5d1adc9c4360b4d51bd1
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2022-05-18 13:13:56 -0700

    First desing in documents

---
+++ app/helpers/DataReplace.js
@@ -0,0 +1,28 @@
+export default function dataReplace(element, url) {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", e => {
+        if (xhr.readyState !== 4) return;
+
+        if (xhr.status >= 200 && xhr.status < 300) {
+            element.outerHTML = xhr.responseText
+            reloadStyles();
+        } else {
+            console.log("Error.responseText")
+        }
+    })
+
+    xhr.open("GET", url)
+
+    xhr.setRequestHeader('content-type', 'text/html; charset=utf-8')
+
+    xhr.send()
+
+}
+
+function reloadStyles() {
+    document.querySelectorAll('pre code').forEach((el) => {
+        hljs.highlightBlock(el)
+    });
+}
--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log("Hola Javascript");
+"use strict"
+import dataReplace from './helpers/DataReplace.js';
+
+document.addEventListener("DOMContentLoaded", e => {
+
+    const replaces = document.querySelectorAll("data-replace")
+
+    replaces.forEach(el => dataReplace(el, el.getAttribute("url")))
+})
